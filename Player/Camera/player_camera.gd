3eef2d6f51949ff342febfe5f845ddb9a7322905
IRVINEUSD\26linyuhangje <Jeremylinyh@gmail.com>
2026-01-13 11:39:33 -0800

    Cam movement and t72 setup

--- Player/Camera/player_camera.gd
+++ Player/Camera/player_camera.gd
@@ -2,7 +2,11 @@ extends Node3D
 
 @export var movementSpeed : float = 100
 
+@export var zoomSpeed : float = 3.0
+
 func _process(delta: float) -> void:
 	var moveDirection : Vector2 = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
-	var movingInDirec : Vector2 = moveDirection*delta * movementSpeed
-	position += Vector3(movingInDirec.x,0.0,movingInDirec.y)
+	var movingInDirec : Vector2 = moveDirection*delta * (movementSpeed + position.y)
+	var deltaH : float = Input.get_axis("zoom_in","zoom_out") * zoomSpeed * delta * 60.0
+	
+	position += Vector3(movingInDirec.x,deltaH,movingInDirec.y)
