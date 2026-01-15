9d5197d16f591352dc70d333d5e143949c297474
IRVINEUSD\26linyuhangje <Jeremylinyh@gmail.com>
2026-01-15 10:54:20 -0800

    Selector UI lol

--- Player/Input/input.gd
+++ Player/Input/input.gd
@@ -1,11 +1,39 @@
-extends Node3D
+extends Panel
+
+var on : bool = true
+var startingPosition : Vector2 = Vector2(0.0,0.0)
+var newPosition : Vector2 = startingPosition
 
 func getRegion() :
+	self.visible = true
+	startingPosition = get_global_mouse_position()
+	self.position = startingPosition
 	while Input.is_action_pressed("Click") :
 		await get_tree().create_timer(0.1).timeout
-	print("released")
+		
+	self.visible = false
+
+func _process(_delta: float) -> void:
+	if not self.visible :
+		return
+	newPosition = get_global_mouse_position()
+	var relativePosition : Vector2 = newPosition-startingPosition
+	
+	if relativePosition.x < 0 :
+		position.x = newPosition.x
+		relativePosition.x *= -1
+	else :
+		position.x = startingPosition.x
+	if relativePosition.y < 0 :
+		position.y = newPosition.y
+		relativePosition.y *= -1
+	else :
+		position.y = startingPosition.y
+	
+	self.size = Vector2(relativePosition)
 
 func _input(event: InputEvent) -> void:
+	if not on :
+		return
 	if event.is_action_pressed("Click") :
-		print("pressed")
 		getRegion()
