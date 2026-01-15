793a5c91470fb779ad48339db72d6db561cd8c67
IRVINEUSD\26linyuhangje <Jeremylinyh@gmail.com>
2026-01-15 12:07:12 -0800

    ???

--- Globals/Globals.gd
+++ Globals/Globals.gd
@@ -1,3 +1,4 @@
 extends Node3D
 
 # global stuff all child can call
+# remeber: premature optimization is the root of all evil
--- Map/AddChunks.gd
+++ Map/AddChunks.gd
@@ -2,9 +2,25 @@
 
 extends Node3D
 @export var terrainViewer : PackedScene
+@export var Heightmap: Texture2D
 
 const tilesNeededEachDir : int = 32
 const tileSize : float = 128.0;
+@onready var heigtmapExample : StaticBody3D = $StaticBody3D
+
+func setCollider(colliderPos : Vector3) :
+	var heightmap := heigtmapExample.duplicate()
+	heightmap.position = colliderPos
+	var shape : CollisionShape3D = heigtmapExample.get_child(0)
+	
+	var map_data = PackedFloat32Array()
+	map_data.resize(pow(tileSize,2.0)) 
+	for x in range(tileSize):
+		for y in range(tileSize):
+			map_data.push_back(0.0)
+			
+	shape.shape.map_data = map_data
+	return heightmap
 
 func _ready() -> void:
 	for x in range(tilesNeededEachDir) :
@@ -14,3 +30,4 @@ func _ready() -> void:
 			newTerrain.position = Vector3((x) * tileSize,0,(y+1)*tileSize)
 			newTerrain.name = str(x) + "," + str(y)
 			newTerrain.owner = null
+			newTerrain.add_child(setCollider(newTerrain.position))
