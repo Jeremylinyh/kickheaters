c0ea6d07bc69e0de3644b7ef6b7663aa62e88740
Jeremylinyh <Jeremylinyh@gmail.com>
2026-01-15 18:29:11 -0800

    Subviewport somewhat setup

---
+++ VisibilityHighlighter/sub_viewport.gd
@@ -0,0 +1,18 @@
+@tool
+extends Node3D
+@export var currentPosition : Transform3D = Transform3D()
+
+var positionPrevFrame : Transform3D = currentPosition
+
+func _ready():
+	$PointOfView.world_3d = get_viewport().find_world_3d()
+	
+
+func _process(_delta: float) -> void:
+	currentPosition = global_transform
+	$PointOfView/Camera3D.global_transform = currentPosition
+	if positionPrevFrame != currentPosition :
+		#print(self.name+" moved")
+		positionPrevFrame = currentPosition
+		$PointOfView.render_target_update_mode = $PointOfView.UPDATE_DISABLED
+		$PointOfView.render_target_update_mode = $PointOfView.UPDATE_ONCE
