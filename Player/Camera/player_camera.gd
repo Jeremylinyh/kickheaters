86c9ebbb5891b1b6d705cefb6b4d2498206c285b
LKQwk <xiexiaochuan22@gmail.com>
2026-02-06 11:50:47 -0800

    added speed up camera
    Added the feature where holding shift allows the camera to be moved faster

--- Player/Camera/player_camera.gd
+++ Player/Camera/player_camera.gd
@@ -6,4 +6,7 @@ func _process(delta: float) -> void:
 	var moveDirection : Vector2 = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
 	var movingInDirec : Vector2 = moveDirection*delta * movementSpeed
 	var moveUpDown : float = Input.get_axis("move_camera_down","move_camera_up")
-	position += Vector3(movingInDirec.x,moveUpDown,movingInDirec.y)
+	if (Input.is_action_pressed("speed_up_camera")):
+		position += 2 * Vector3(movingInDirec.x,moveUpDown,movingInDirec.y)
+	else:	
+		position += Vector3(movingInDirec.x,moveUpDown,movingInDirec.y)
