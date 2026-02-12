3a20b9edf580e38d1904ffdf5beea69d7ab043fe
LKQwk <xiexiaochuan22@gmail.com>
2026-02-12 12:02:50 -0800

    Add Sound Effect
    Added the awp sound effect from CSGO  as the firing sound for the tank

--- Assets/ParticleEffects/muzzle_flash.gd
+++ Assets/ParticleEffects/muzzle_flash.gd
@@ -4,6 +4,7 @@ extends Node3D
 func muzzleFlash():
 	$Flame.emitting = true;
 	$Smoke.emitting = true;
+	$ExplosionSound.play()
 	await get_tree().create_timer(4.0).timeout
 	$Flame.emitting = false;
 	$Smoke.emitting = false;
